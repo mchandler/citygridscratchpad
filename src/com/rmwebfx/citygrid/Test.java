800f1eb66495245a2166a20d7707631ee9965a4b
Mike Chandler <mike@rmwebfx.com>
2012-03-03 20:45:07 -0800

    More testing.

--- src/com/rmwebfx/citygrid/Test.java
+++ src/com/rmwebfx/citygrid/Test.java
@@ -2,6 +2,10 @@ package com.rmwebfx.citygrid;
 
 import com.citygrid.CGException;
 import com.citygrid.CityGrid;
+import com.citygrid.CGBaseReview;
+import com.citygrid.ads.custom.CGAdsCustom;
+import com.citygrid.ads.custom.CGAdsCustomAd;
+import com.citygrid.ads.custom.CGAdsCustomResults;
 import com.citygrid.content.places.search.*;
 import com.citygrid.content.places.detail.*;
 
@@ -9,7 +13,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		//testPlacesSearch();
-		testPlace();
+		//testPlace();
+		testAd();
 	}
 	
 	public static void testPlacesSearch() {
@@ -41,16 +46,49 @@ public class Test {
 		CityGrid.setSimulation(false);
 		
 		CGPlacesDetail detail = CityGrid.placesDetail();
-		detail.setLocationId(35777906);
+		detail.setLocationId(664259190);
 		
 		try {
 			CGPlacesDetailResults results = detail.detail();
 			CGPlacesDetailLocation location = results.getLocation();
-			CGPlacesDetailReviews reviews = location.getReviews();
+			CGPlacesDetailReviews reviewContainer = location.getReviews();
+			CGBaseReview[] reviews = reviewContainer.getReviews();
 			System.out.println("Worked! This is " + location.getName());
-			System.out.println("Total Reviews: " + new Integer(reviews.getCount()).toString());
+			System.out.println("Total Reviews: " + new Integer(reviewContainer.getCount()).toString());
+			System.out.println("Rating: " + new Integer(location.getRating()).toString());
+			System.out.println("Hours: " + location.getBusinessHours());
+			
+			CGBaseReview review = reviews[0];
+			System.out.println(review.getTitle());
+			System.out.println(review.getText());
+			System.out.println(review.getRating());
 		} catch (CGException e) {
 			System.out.println("Hosed: " + e);
 		}
+	}
+	
+	public static void testAd() {
+		CityGrid.setPublisher("10000002304");
+		CityGrid.setSimulation(true);
+		
+		CGAdsCustom search = CityGrid.adsCustom();
+		search.setWhat("restaurant");
+		search.setWhere("Simi Valley, CA");
+		search.setRadius(30.0f);
+		
+		try {
+			CGAdsCustomResults results = search.search();
+			CGAdsCustomAd[] ads = results.getAds(); //sometimes there are no ad matches!
+			
+			if (ads.length > 0) {
+				CGAdsCustomAd ad = results.getAd();
+				System.out.println("Ad for: " + ad.getName());
+			}
+			
+		} catch (CGException e) {
+			System.out.println("Poof: " + e);
+		}
+		
+		
 	}
 }
